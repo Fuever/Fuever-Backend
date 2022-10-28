101b661a70651882c43f69b87ab9913fcb68c0e7
Pigeon377 <892197240@qq.com>
2022-10-28 23:29:34 +0800

    style: rename DB => db

--- model/db.go
+++ model/db.go
@@ -16,15 +16,20 @@ const (
 )
 
 var (
-	DB  *gorm.DB = nil
+	db  *gorm.DB = nil
 	dsn          = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, databaseName, port)
 )
 
 func InitDB() {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbEngine, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		// can't connect database
 		log.Fatalln(err)
 	}
-	DB = db
+	db = dbEngine
+	// automatically generate database tables
+	err = db.AutoMigrate()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
