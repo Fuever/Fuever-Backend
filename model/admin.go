c82febc948302719cdac9c2517f02933125ea69f
Pigeon377 <892197240@qq.com>
2022-10-30 10:13:20 +0800

    feat(model): implement admin.go

--- model/admin.go
+++ model/admin.go
@@ -6,6 +6,14 @@ type Admin struct {
 	Password string `gorm:"varchar(64);not null"`
 }
 
+func CreateAdmin(admin *Admin) error {
+	err := db.Create(admin).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func GetAdminByID(id int) (*Admin, error) {
 	admin := &Admin{ID: id}
 	err := db.Find(admin).Error
@@ -13,4 +21,20 @@ func GetAdminByID(id int) (*Admin, error) {
 		return nil, err
 	}
 	return admin, nil
+}
+
+func UpdateAdmin(admin *Admin) error {
+	err := db.Where("id = ?", admin.ID).Updates(admin).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteAdminByID(id int) error {
+	err := db.Delete(&Admin{}, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
