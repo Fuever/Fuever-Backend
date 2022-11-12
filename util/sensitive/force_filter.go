c9cf0db42160dd538c5dfa33f95bed641c4e2efb
garliczz <1214156779@qq.com>
2022-11-12 21:30:10 +0800

    fix(force_filter):*

--- util/sensitive/force_filter.go
+++ util/sensitive/force_filter.go
@@ -1,45 +1,41 @@
 package sensitive
 
 import (
-	"os"
+	"Fuever/resource"
 	"sort"
 	"strings"
 )
 
 type ForceMatchFilter struct {
-	SensitiveWords []string //敏感词数组
+	sensitiveWords []string //敏感词数组
 }
 
-func (it *ForceMatchFilter) IsSensitive(Text string) bool {
-	for _, SensitiveWord := range it.SensitiveWords { //检查是否存在敏感词
-		if strings.Contains(Text, SensitiveWord) {
+func (it *ForceMatchFilter) IsSensitive(text string) bool {
+	for _, SensitiveWord := range it.sensitiveWords { //检查是否存在敏感词
+		if strings.Contains(text, SensitiveWord) {
 			return true
 		}
 	}
 	return false
 }
 
-func (it *ForceMatchFilter) ReplaceSensitiveWord(Text string, ReplaceString string) string {
-	for _, SensitiveWord := range it.SensitiveWords { //替换敏感词
+func (it *ForceMatchFilter) ReplaceSensitiveWord(text string, replaceString string) string {
+	for _, SensitiveWord := range it.sensitiveWords { //替换敏感词
 		if len(SensitiveWord) != 0 {
-			Text = strings.Replace(Text, SensitiveWord, ReplaceString, -1)
+			text = strings.Replace(text, SensitiveWord, replaceString, -1)
 		}
 	}
-	return Text
+	return text
 }
 
 func (it *ForceMatchFilter) readSensitiveWord() []string {
-	Text, err := os.ReadFile("../../resource/广告.txt")
-	if err != nil {
-		panic(err)
-	}
-	return strings.Split(string(Text), "\n")
+	return resource.SensitiveWords()
 }
 
 func (it *ForceMatchFilter) InitFilter() error {
-	it.SensitiveWords = it.readSensitiveWord()          //初始化敏感词数组
-	sort.Slice(it.SensitiveWords, func(i, j int) bool { //按照敏感词长度降序排列
-		return len(it.SensitiveWords[i]) < len(it.SensitiveWords[j])
+	it.sensitiveWords = it.readSensitiveWord()          //初始化敏感词数组
+	sort.Slice(it.sensitiveWords, func(i, j int) bool { //按照敏感词长度降序排列
+		return len(it.sensitiveWords[i]) > len(it.sensitiveWords[j])
 	})
 	return nil
 }
