aec22113ffbf89128d5caba198f1b7a171b1f868
garliczz <1214156779@qq.com>
2022-11-12 22:14:47 +0800

    fix(force_filter):QQ->**

--- util/sensitive/filter_test.go
+++ util/sensitive/filter_test.go
@@ -9,18 +9,30 @@ func TestFilter(t *testing.T) {
 	filter := ForceMatchFilter{}
 	err := filter.InitFilter()
 	if err != nil {
-		t.Error("读入敏感词失败")
+		t.Error("暴力	读入敏感词失败")
+	}
+	acFilter := AcAutomaton{}
+	err = acFilter.InitFilter()
+	if err != nil {
+		t.Error("自动机读入敏感词失败")
 	}
 
 	if !filter.IsSensitive("扣扣") {
-		t.Error("检测敏感词失败")
+		t.Error("暴力检测敏感词失败")
+	}
+	if !acFilter.IsSensitive("扣扣") {
+		t.Error("自动机检测敏感词失败")
 	}
 
-	text := filter.ReplaceSensitiveWord("兼职扣扣", "*")
-	if text != "****" {
+	content := "Q兼职扣扣淘宝Q"
+	replaceString := "哈哈哈"
+	text := filter.ReplaceSensitiveWord(content, replaceString)
+	acText := acFilter.ReplaceSensitiveWord(content, replaceString)
+
+	if text != acText {
 		t.Error("替换敏感词失败")
-		fmt.Println("期望输出：****")
-		fmt.Println("实际输出：" + text)
+		fmt.Println("暴力输出：" + text)
+		fmt.Println("自动机输出：" + acText)
 	}
 
 }
