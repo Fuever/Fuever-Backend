c49a98bd06ac5078a9a28e515556c6d086ad1eec
Pigeon377 <892197240@qq.com>
2022-11-12 14:41:32 +0800

    feat(secret): add authentication method

--- cmd/boot.go
+++ cmd/boot.go
@@ -3,13 +3,18 @@ package main
 import (
 	"Fuever/model"
 	"Fuever/router"
+	"Fuever/util/secret"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	model.InitDB()
 	g := gin.Default()
+
 	router.InitRoute(g)
+	model.InitDB()
+	secret.InitTokenCache()
+
 	err := g.Run(":8080")
 	if err != nil {
 		panic(err)
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-playground/assert/v2 v2.0.1
 	//github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-redis/redis/v8  v8.11.5
 	gorm.io/driver/postgres v1.3.4
 	gorm.io/gorm v1.23.8
 )
