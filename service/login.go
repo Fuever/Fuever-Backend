8f234ad1f6b1b99b60a8005f0aef6cbb0b7e3ac0
Pigeon377 <892197240@qq.com>
2022-11-13 10:03:45 +0800

    feat(middleware): implement auth middleware

--- service/login.go
+++ service/login.go
@@ -2,11 +2,15 @@ package service
 
 import (
 	"Fuever/util/secret"
+	"strconv"
 )
 
 // Login 返回Token
+// 这里的token实际上是
+// userID@token的形式
+// 我不知道这算不算一个馊主意
 func Login(userID int) string {
-	return secret.GenerateTokenAndCache(userID)
+	return strconv.Itoa(userID) + "@" + secret.GenerateTokenAndCache(userID)
 }
 
 func Logout(userID int) {
