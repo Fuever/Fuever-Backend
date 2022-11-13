390db249dbe3d745b22886a907188ebf7eb48221
Pigeon377 <892197240@qq.com>
2022-11-13 09:28:03 +0800

    feat(secret): add logout method

--- service/login.go
+++ service/login.go
@@ -1,7 +1,14 @@
 package service
 
-//type Token = string
+import (
+	"Fuever/util/secret"
+)
 
-func LoginService() []string {
-	return nil
+// Login 返回Token
+func Login(userID int) string {
+	return secret.GenerateTokenAndCache(userID)
+}
+
+func Logout(userID int) {
+	secret.RemoveTokenFromCache(userID)
 }
