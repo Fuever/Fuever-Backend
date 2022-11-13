16dfe3ff4271181c9736738ac26e2b0be4cd14b8
Pigeon377 <892197240@qq.com>
2022-11-13 10:38:30 +0800

    style(resource): rename stu.csv => stu.dat

--- resource/stumsg.go
+++ resource/stumsg.go
@@ -15,7 +15,7 @@ type StudentMessage struct {
 	Bed       int // A -> 1, B -> 2, C -> 3, D -> 4
 }
 
-//go:embed stu.csv
+//go:embed stu.dat
 var studentMessageString string
 
 func StudentMessages() []*StudentMessage {
