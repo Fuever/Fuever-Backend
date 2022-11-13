0494d354121cf0fdde1e24c975ea9aaa812d99bc
Pigeon377 <892197240@qq.com>
2022-11-13 08:53:32 +0800

    test(resource): add resource_test

--- resource/resource_test.go
+++ resource/resource_test.go
@@ -1,1 +1,19 @@
 package resource
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSensitiveWords(t *testing.T) {
+	for _, v := range SensitiveWords() {
+		assert.True(t, v != "")
+	}
+}
+
+func TestStudentMessages(t *testing.T) {
+	for _, v := range StudentMessages() {
+		assert.True(t, v.Number != 0)
+		assert.True(t, v.Name != "")
+	}
+}
