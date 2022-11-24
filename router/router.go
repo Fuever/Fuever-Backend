6d3104110cad259ab70ee4e2e3fb36e0d65c7923
Pigeon377 <892197240@qq.com>
2022-11-24 11:28:31 +0800

    fix(router): change cors policy

--- router/router.go
+++ router/router.go
@@ -2,24 +2,14 @@ package router
 
 import (
 	"Fuever/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(g *gin.Engine) {
 
+	g.Use(cors.Default())
 	api := g.Group("/api")
-	//api.Use(cors.Default())
-	api.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
 	{
 		auth := api.Group("/auth")
 		{
