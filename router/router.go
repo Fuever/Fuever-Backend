3fdc5da5a844a38c2078d42930099fc85a019e86
Pigeon377 <892197240@qq.com>
2022-11-24 11:42:36 +0800

    fix(router): remove cors middleware and add cors config

--- router/router.go
+++ router/router.go
@@ -2,13 +2,22 @@ package router
 
 import (
 	"Fuever/middleware"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(g *gin.Engine) {
 
-	g.Use(cors.Default())
+	g.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 	api := g.Group("/api")
 	{
 		auth := api.Group("/auth")
