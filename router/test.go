7f64064ae888ea2e0df56728437aa67b3a1dfe28
Pigeon377 <892197240@qq.com>
2022-12-16 18:29:26 +0800

    test(router): add test case

--- router/test.go
+++ router/test.go
@@ -34,11 +34,63 @@ func GenerateTest() {
 	if err != nil {
 		panic(err)
 	}
+	err = model.CreateUser(&model.User{
+		Mail:         "123456",
+		Password:     repassword.GeneratePasswordHash("12345"),
+		Username:     "chen jia long",
+		Nickname:     "龙除",
+		Avatar:       "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1669256053941570786.jpg",
+		StudentID:    32004106,
+		Phone:        1145141919,
+		Gender:       true,
+		Age:          21,
+		Job:          "击而破之",
+		EntranceTime: time.Now().Unix(),
+		Residence:    "福州",
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = model.CreateUser(&model.User{
+		Mail:         "123457",
+		Password:     repassword.GeneratePasswordHash("12345"),
+		Username:     "小姨",
+		Nickname:     "老姨",
+		Avatar:       "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1669256053941570786.jpg",
+		StudentID:    32004120,
+		Phone:        7878787,
+		Gender:       true,
+		Age:          90,
+		Job:          "地主",
+		EntranceTime: time.Now().Unix(),
+		Residence:    "地里",
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = model.CreateUser(&model.User{
+		Mail:         "123458",
+		Password:     repassword.GeneratePasswordHash("12345"),
+		Username:     "鱼除",
+		Nickname:     "打个胶先",
+		Avatar:       "https://fuever-1313037799.cos.ap-nanjing.myqcloud.com/1669256053941570786.jpg",
+		StudentID:    32004132,
+		Phone:        114514191981,
+		Gender:       true,
+		Age:          22,
+		Job:          "胶工",
+		EntranceTime: time.Now().Unix(),
+		Residence:    "孙吧",
+	})
+	if err != nil {
+		panic(err)
+	}
 
 	blockNumber := 5
 	postNumber := 300
 	commentNumber := 5000
 	classNumber := 10
+	stuNumArray := []int{32004122, 32004106, 32004132, 32004120}
 
 	for i := 0; i < blockNumber; i++ {
 		model.CreateBlock(&model.Block{
@@ -49,7 +101,7 @@ func GenerateTest() {
 
 	for i := 0; i < postNumber; i++ {
 		model.CreatePost(&model.Post{
-			AuthorID:    1,
+			AuthorID:    rand.Intn(4) + 1,
 			Title:       strconv.Itoa(rand.Int()),
 			CreatedTime: time.Now().Unix() + int64(rand.Intn(114)*int(time.Second.Seconds())),
 			UpdatedTime: time.Now().Unix() + int64(rand.Intn(114)*int(time.Second.Seconds())),
@@ -61,7 +113,7 @@ func GenerateTest() {
 
 	for i := 0; i < commentNumber; i++ {
 		model.CreateMessage(&model.Message{
-			AuthorID:    1,
+			AuthorID:    rand.Intn(4) + 1,
 			Content:     strconv.Itoa(rand.Int()),
 			PostID:      rand.Intn(postNumber),
 			CreatedTime: time.Now().Unix() + int64(rand.Intn(114)*int(time.Second.Seconds())),
@@ -108,9 +160,22 @@ func GenerateTest() {
 		model.CreateGallery(gallery)
 	}
 	for i := 0; i < classNumber; i++ {
+		clsName := strconv.Itoa(rand.Int())
 		model.CreateClass(&model.Class{
-			ClassName: strconv.Itoa(rand.Int()),
-			StudentID: 32004122,
+			ClassName: clsName,
+			StudentID: stuNumArray[rand.Intn(4)],
+		})
+		model.CreateClass(&model.Class{
+			ClassName: clsName,
+			StudentID: stuNumArray[rand.Intn(4)],
+		})
+		model.CreateClass(&model.Class{
+			ClassName: clsName,
+			StudentID: stuNumArray[rand.Intn(4)],
+		})
+		model.CreateClass(&model.Class{
+			ClassName: clsName,
+			StudentID: stuNumArray[rand.Intn(4)],
 		})
 	}
 
